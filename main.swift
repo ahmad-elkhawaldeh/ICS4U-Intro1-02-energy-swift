1777665aacd38e9f15837193e48d91d65efedfef
ahmadelkhawalde <60712751+ahmad-elkhawaldeh@users.noreply.github.com>
2021-11-29 17:38:08 +0000

    "catch3"

--- main.swift
+++ main.swift
@@ -34,8 +34,7 @@ do {
     let lightSpeed = (POW4 * pow(POW2, POW3))
     let energy = pow(lightSpeed, POW1)*(massFloat)
     print("If \(mass ?? "00") kg of mass was converted to energy , it would produce \(energy) J")
-}
-   catch {
-       print("ERROR NUMBBER NOT VALID TRY AGAIN")
-       print("\nDone")
+} catch {
+      print("ERROR NUMBBER NOT VALID TRY AGAIN")
+      print("\nDone")
 }
