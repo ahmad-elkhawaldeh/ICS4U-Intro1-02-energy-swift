a9625561cadb57e13115f1a09b2994e7289b3b6f
ahmadelkhawalde <60712751+ahmad-elkhawaldeh@users.noreply.github.com>
2021-11-29 15:25:41 +0000

    "catch statment"

--- main.swift
+++ main.swift
@@ -8,6 +8,7 @@
 //                         
 //  This program calculates the amount of energy
 //
+
 import Foundation
 
 // https://stackoverflow.com/questions/60788627/custom-errormessage-on-enum-field-upon-failed-validation-in-spring
@@ -33,7 +34,8 @@ do {
     let lightSpeed = (POW4 * pow(POW2, POW3))
     let energy = pow(lightSpeed, POW1)*(massFloat)
     print("If \(mass ?? "00") kg of mass was converted to energy , it would produce \(energy) J")
-}catch {
+}
+catch {
     print("ERROR NUMBBER NOT VALID TRY AGAIN")
     print("\nDone")
 }
